b4413ef2ee19bbd82ee21b26b56f1758f8c7f6f7
Prakash Bhatt <prk-jr@Prakashs-MacBook-Air.local>
2025-01-06 18:50:10 +0530

    WIP

--- src/template.rs
+++ src/template.rs
@@ -11,7 +11,7 @@ use convert_case::Casing;
 use std::fs;
 
 fn create_file(project_name: &str, file_path: &str, content: &str) -> std::io::Result<()> {
-    let path = format!("{}/{}", project_name, file_path.replace("{", "_by_").replace("}", ""));
+    let path = format!("{}/{}", project_name, file_path.replace("{", "by_").replace("}", ""));
     fs::write(path, content)
 }
 
@@ -85,7 +85,7 @@ pub fn create_router(config: &Config) -> String {
                         .to_lowercase()
                         .replace("/", "_")
                         .replace(":", "")
-                        .replace("{", "_by_")
+                        .replace("{", "by_")
                         .replace("}", ""),
                 );
                 let functions2 = format!(
@@ -97,7 +97,7 @@ pub fn create_router(config: &Config) -> String {
                         .to_lowercase()
                         .replace("/", "_")
                         .replace(":", "")
-                        .replace("{", "_by_")
+                        .replace("{", "by_")
                         .replace("}", ""),
                 );
                 if endpoint.method.to_lowercase() == "get" {
@@ -152,7 +152,7 @@ pub fn modify_files(project_name: &str, config: &Config) -> std::io::Result<()> 
                 let file = format!(
                     "{}{}",
                     endpoint_type,
-                    path.replace("/", "_").replace(":", "").replace("{", "_by_").replace("}", "")
+                    path.replace("/", "_").replace(":", "").replace("{", "by_").replace("}", "")
                 );
                 endpoint_files.push(file.clone());
                 if endpoint.method.to_lowercase() == "get" {
